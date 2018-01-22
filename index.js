02753accb3166a68a7ef041d36f96e8150dcd663
Christine Prevel <christine.prevel@gmail.com>
2018-01-22 21:22:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}';
+  return `Happy holidays, ${name}`;
 }
 
 function happyHolidayTo(holiday,name) {
-  return 'Happy ${holiday}, ${name}!'; 
+  return `Happy ${holiday}, ${name}!`; 
 }
 
 function holidayCountdown(holidayName,daysTilHoliday) {
-  return 'It\'s ${daysTilHoliday} days until ${holidayName}!';
+  return `It\'s ${daysTilHoliday} days until ${holidayName}!`;
 }
